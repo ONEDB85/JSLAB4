41b0d97305d097cba6ded6576810851e74647ff7
D'Lorean Bell <dloreanbell@gmail.com>
2018-02-03 11:54:10 -0500

    game modifications

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 (function() {
-    
+
     var startGame = document.getElementById("hidden");
     var startButton = document.getElementById("startButton");
     var quitButton = document.getElementById("quitButton");
 
     var attackButton = document.getElementById("attackButton");
-    
+
     var userHealthBar = document.getElementById("userHealth");
     var computerHealthBar = document.getElementById("computerHealth");
     var winsBar = document.getElementById("userWins");
@@ -14,11 +14,11 @@
     var messageE2 = document.getElementById("message2");
     var name = document.getElementById("name");
     var quitter = document.getElementById("quitter");
-
+	var samurai = document.getElementById("samurai");
 
     var userHeals = document.getElementById("userHeals");
 
-    var user =   {    
+    var user = {
         name: "",
         health: 40,
         healsRemaining: 2,
@@ -30,11 +30,9 @@
         heal: function() {
             this.health += Math.floor(Math.random() * 10) + 1;
         },
-
-      
     }
 
-   var computer = {
+    var computer = {
 
         name: "Grant the Almighty Chicken",
         health: 10,
@@ -48,84 +46,79 @@
     startButton.onclick = function() {
 
         user.name = prompt("What will be your name?");
-        
+
         startButton.style.display = 'none';
-        startGame.classList.remove("main"); 
+        startGame.classList.remove("main");
         updateName("Name: " + user.name);
 
 
 
-    };    
+    };
 
     attackButton.onclick = function() {
-        if(user.health > 0 && computer.health > 0){
-        
-        user.health -= computer.generateAttackDamage();
-        computer.health -= user.generateAttackDamage();
-        
-       
-        updateAttackReceived();
-        updateAttackGiven();
+        if (user.health > 0 && computer.health > 0) {
+
+            user.health -= computer.generateAttackDamage();
+            computer.health -= user.generateAttackDamage();
 
 
-         
-        updateMessage(user.name + " has " + user.health + " health left. "+
-            computer.name + " has " + computer.health + " health left." );
-        updateMessage1("");
+            updateAttackReceived();
+            updateAttackGiven();
 
-        
-        if (user.health <= 0) {
+
+
+            updateMessage(user.name + " has " + user.health + " health left. " +
+                computer.name + " has " + computer.health + " health left.");
+            updateMessage1("");
+
+
+            if (user.health <= 0) {
 
                 updateMessage(computer.name + " Wins!");
                 updateMessage1("GAME OVER!");
-             
+
             }
 
-        if (computer.health <= 0) {
+            if (computer.health <= 0) {
                 computer.health += 10;
                 user.wins++;
                 addWins();
 
-             if (user.wins >= 5) {
+                if (user.wins >= 5) {
 
                     updateMessage(user.name + " Wins!");
                     updateMessage1("GAME OVER!");
-                   
-                }  
-
+                }
             }
-
-            }   
-        
-
+        }
     };
 
     healButton.onclick = function() {
-    
 
-    if(user.health > 0 && computer.health > 0){   
-    if (user.healsRemaining > 0){
-        user.heal();
-        user.healsRemaining--;
-        updateHealRepair();
-        updateMessage(user.name + " has healed and has " + user.health + " health left.");
-        //updateAttackReceived();
-        updateMessage1("");
-        }else{
+        if (user.health > 0 && computer.health > 0) {
+            if (user.healsRemaining > 0) {
+                user.heal();
+                user.healsRemaining--;
+                updateHealRepair();
+                updateMessage(user.name + " has healed and has " + user.health + " health left.");
+                //updateAttackReceived();
+                updateMessage1("");
+            } else {
 
-            updateMessage(user.name + " has no more heals!");
-            updateMessage1("Get your mind right!");
+                updateMessage(user.name + " has no more heals!");
+                updateMessage1("Get your mind right!");
+            }
         }
-    }
     };
 
-    quitButton.onclick = function(){
-    
-      if(user.health > 0 && computer.health > 0){
-       gameOver("GAME OVER YOU QUIT! BE GONE!");
-       quitButton.style.display = 'none';
-       startGame.classList.add("main");
-                   }
+    quitButton.onclick = function() {
+
+        if (user.health > 0 && computer.health > 0) {
+            gameOver("GAME OVER YOU QUIT! BE GONE!");
+            quitButton.style.display = 'none';
+            startGame.classList.add("main");
+            samurai.classList.remove("beGone");
+        }
 
     };
 
@@ -142,8 +135,8 @@
         computerHealthBar.value = computer.health;
     }
 
-     function addWins() {
-       winsBar.value =  user.wins;
+    function addWins() {
+        winsBar.value = user.wins;
     }
 
     function updateMessage(newMessage) {
